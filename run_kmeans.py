7655e2efa563828a412389b995d0e1f919f01413
Kur0koZ <weerapats100@gmail.com>
2026-08-12 15:59:51 +0700

    Update run_kmeans.py

--- run_kmeans.py
+++ run_kmeans.py
@@ -1,29 +1,31 @@
 import numpy as np
        # x,y,r,g,b
-data = [[3,2], 
-        [6,3], 
-        [2,4], 
-        [7,4], 
-        [8,5], 
-        [3,7], 
-        [6,8], 
-        [5,9], 
-        [2,9]
+data = [[193,131,226,243,242], 
+        [483,85,0,155,255], 
+        [449,263,232,252,200], 
+        [479,497,91,82,121], 
+        [551,553,108,101,135], 
+        [679,483,255,240,172], 
+        [685,485,255,191,51], 
+        [751,513,207,147,37], 
+        [611,641,0,141,255],
+        [799,753,233,255,207]
         ]
 
 data = np.array(data, dtype=np.float64)
 numD = data.shape[0]
 print("Data Count:", numD)
 
-k = 2
-C = [[2,6],
-     [6,5]
+k = 3
+C = [[627,743,106,179,59],
+     [609,889,253,243,237],
+     [613,991,252,213,136]
      ]
 C = np.array(C, dtype=np.float64)
 numC = C.shape[0]
 
 distance = np.zeros((numD, k))
-iter = 2
+iter = 20
 
 for it in range(iter):
     for cntD in range(numD):
@@ -37,6 +39,7 @@ for it in range(iter):
             wcss += np.sum(np.linalg.norm(data[indices_of_C] - C[cntC], axis=1)**2)
             C[cntC] = np.mean(data[indices_of_C], axis=0)
 
-print("Iteration 1")
-print("Labels:", labels)
+    print("Iteration ",it)
+    print("Labels:", labels)
+    print("WCSS:", wcss)
 print("WCSS:", wcss)
